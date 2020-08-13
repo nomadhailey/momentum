5a2098abc36ec34c0eabe66b23d2b15597cbd4c1
= <yyb2535@gmail.com>
2020-08-14 02:20:08 +0900

    settings

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, BrowserRouter } from "react-router-dom";
 import { ErrorBoundary } from "react-error-boundary";
-import { ConnectedRouter } from "connected-react-router";
 import FatalError from "./pages/FatalError";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
-import { history } from "./redux/create";
 
 function App() {
   return (
     <ErrorBoundary FallbackComponent={FatalError}>
-      <ConnectedRouter history={history}>
+      <BrowserRouter>
         <Switch>
           <Route path="/" exact component={Home} />
           <Route component={NotFound} />
         </Switch>
-      </ConnectedRouter>
+      </BrowserRouter>
     </ErrorBoundary>
   );
 }
--- src/services/WeatherService.js
+++
@@ -1,4 +0,0 @@
-import axios from "axios";
-
-const API_KEY = "64bb4412769a73988b668782663bd5f7";
-const week = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
